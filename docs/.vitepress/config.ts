7d96e30940f8269b15f1f1893b39d800999b1a45
JustinJsn <1278164528@qq.com>
2022-06-23 16:14:13 +0800

    feat: 补充vitepress配置

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,12 +2,24 @@ import nav from "./config/nav";
 import { sidebarDataStructure } from "./config/sidebar";
 
 export default {
-  title: "blog",
+  title: "JustinJsn博客",
   lastUpdated: true,
   themeConfig: {
     nav: nav(),
     sidebar: {
       "/大话数据结构": sidebarDataStructure(),
     },
+    footer: {
+      message: "MIT License",
+      copyright: `Copyright © 2022-present JustinJsn`,
+    },
+    editLink: {
+      pattern:
+        "https://github.com/JustinJsn/vitepress-blog/edit/master/docs/:path",
+      text: "Edit this page on GitHub",
+    },
+    socialLinks: [
+      { icon: "github", link: "https://github.com/JustinJsn/vitepress-blog" },
+    ],
   },
 };
