847dff6f1aaf29bb8b05242dcb5ddcd63d698c73
JustinJsn <1278164528@qq.com>
2022-07-16 11:18:23 +0800

    feat: 推送gitee

--- deploy.sh
+++ deploy.sh
@@ -9,11 +9,15 @@ npm run build
 # 进入生成的文件夹
 cd docs/.vitepress/dist
 
+# 拷贝目录和文件
+cp -r ../../../.github ./
+
 git init
 git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:JustinJsn/vitepress-blog.git master:gh-pages
+git push -f git@gitee.com:justinjsn/vitepress-blog.git master:gh-pages
 
 cd -
